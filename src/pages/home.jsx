2ea44f209e49da2b7a55023a79a5e8f77f6c5106
Abhinav Gupta <abhinav30g@gmail.com>
2024-12-30 15:20:49 +0530

    small change

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,8 +7,9 @@ function Home() {
       {/* <h1 className="text-5xl bg-slate-700">This is the home page.</h1> */}
       {/* Div changed to properly show header in the home page */}
       {/* <div className="fixed inset-0 overflow-hidden"> */}
-      <div className="relative h-full">  
-      <Office/>
+
+      <div className="relative h-full">
+        <Office />
       </div>
     </div>
   );
